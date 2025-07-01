b436389fd5a2fe44459990971fd8a37574e7d438
unknown <quydnvn@gmail.com>
2025-07-01 07:20:06 +0700

    init Project

---
+++ AirlineWeb/Profiles/WebhookSubscriptionProfile.cs
@@ -0,0 +1,18 @@
+
+using AutoMapper;
+using AirlineWeb.Dtos;
+using AirlineWeb.Models;
+
+namespace AirlineWeb.Profiles
+{
+    public class WebhookSubscriptionProfile : Profile
+    {
+        public WebhookSubscriptionProfile()
+        {
+            //Source -> Target
+            CreateMap<WebhookSubscriptionCreateDto, WebhookSubscription>();
+            CreateMap<WebhookSubscription, WebhookSubscriptionReadDto>();
+            
+        }
+    }
+}
---
+++ TravelAgentWeb/Controllers/NotificationsController.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TravelAgentWeb.Data;
+using TravelAgentWeb.Dtos;
+
+namespace TravelAgentWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : ControllerBase
+    {
+        private readonly TravelAgentDbContext _context;
+
+        public NotificationsController(TravelAgentDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public ActionResult FlightChanged(FlightDetailUpdateDto flightDetailUpdateDto)
+        {
+            Console.WriteLine($"Webhook Receieved from: {flightDetailUpdateDto.Publisher}");
+
+            var secretModel = _context.SubscriptionSecrets.FirstOrDefault(s => 
+                s.Publisher == flightDetailUpdateDto.Publisher && 
+                s.Secret == flightDetailUpdateDto.Secret);
+
+            if (secretModel == null)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid Secret - Ignore Webwook");
+                Console.ResetColor();
+                return Ok();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Valid Webhook!");
+                Console.WriteLine($"Old Price {flightDetailUpdateDto.OldPrice}, New Price {flightDetailUpdateDto.NewPrice}");
+                Console.ResetColor();
+                return Ok();
+            }
+        }
+        
+
+    }
+}
